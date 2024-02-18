e7e0ac156fabb72146ddec49869ff58b07c7e0c9
Orzech <hoch.mat@gmail.com>
2024-02-18 13:16:24 +0100

    Removed unnecessary fields from DTOs

--- src/main/java/com/mhoch/githubuser/domain/dto/BranchDto.java
+++ src/main/java/com/mhoch/githubuser/domain/dto/BranchDto.java
@@ -1,6 +1,5 @@
 package com.mhoch.githubuser.domain.dto;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Builder;
 import lombok.Value;
 
@@ -9,6 +8,4 @@ import lombok.Value;
 public class BranchDto {
     String name;
     CommitDto commit;
-    @JsonProperty("protected")
-    boolean isProtected;
 }
